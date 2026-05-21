c4093f7d4124e1362ebabacfa97b190961052145
dwtegarsaputra-prog <dwtegarsaputra@gmail.com>
2026-05-22 00:46:53 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,27 +1,27 @@
 <?php
-// Pengaturan Database Railway
+// Data Database Railway
 $host = "kodama.proxy.rlwy.net"; 
 $user = "root";                  
 $pass = "BTDqJLWHKlclZxkgVIGnkWOjLAbrVoab"; 
 $db   = "railway";               
 $port = "22092";                 
 
-// Simpan variabel ke konstanta agar webhook.php tidak bingung
+// Mendefinisikan konstanta agar dikenali oleh webhook.php
 define('DB_HOST', $host);
 define('DB_USER', $user);
 define('DB_PASS', $pass);
 define('DB_NAME', $db);
 define('DB_PORT', $port);
 
-// Koneksi PDO (Gunakan port)
+// Koneksi PDO (Menggunakan Port Railway)
 try {
     $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT, DB_USER, DB_PASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Koneksi gagal: " . $e->getMessage());
+    // Jika gagal konek, biarkan aplikasi tetap jalan tapi catat error
 }
 
-// Telegram Bot
+// Token Telegram
 define('TELEGRAM_TOKEN', '8749239208:AAFyJIvjIhGT9VpEW65nDtyLjSAwXa2YHao');
 define('TELEGRAM_API', 'https://api.telegram.org/bot'.TELEGRAM_TOKEN.'/');
 ?>
