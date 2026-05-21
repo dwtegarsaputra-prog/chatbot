2471206a36d25bd77a9415c796f47319b69dd3ad
dwtegarsaputra-prog <dwtegarsaputra@gmail.com>
2026-05-22 00:10:42 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,18 +1,16 @@
 <?php
-// Pengaturan Database Railway
 $host = "kodama.proxy.rlwy.net"; 
 $user = "root";                  
 $pass = "BTDqJLWHKlclZxkgVIGnkWOjLAbrVoab"; 
 $db   = "railway";               
 $port = "22092";                 
 
-// Koneksi mysqli (untuk admin/web jika ada)
 $koneksi = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 
-// Pengaturan API Telegram (PENTING untuk webhook.php)
+// Tambahkan baris ini di bawah agar webhook.php tidak error!
 define('TELEGRAM_API', 'https://api.telegram.org/bot8749239208:AAFyJIvjIhGT9VpEW65nDtyLjSAwXa2YHao/');
 ?>
